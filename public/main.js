de6dd5b9f87cbcf0990402f2e76da6dd766fe65f
rinarive <rinarive@gmail.com>
2020-05-25 22:05:38 -0400

    primera version

---
+++ public/main.js
@@ -0,0 +1,44 @@
+
+let personas=[]
+const card = document.getElementById("root"); 
+let searchPeli = document.getElementById('search').addEventListener('click', (e) => {
+
+fetch("https://randomuser.me/api/?results=20")
+  .then(datos => datos.json() )
+  .then(data=>{
+    console.log(data)
+    let search = data.results
+
+   search.forEach(element => {
+     
+      card.innerHTML= `
+      <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img src="${element.picture.large}" alt="Placeholder image">
+          </figure>
+      </div>
+      <div class="card-content">
+        <div class="media-content">
+          <p class="title is-4">${element.name.first}</p>
+          <p class="subtitle is-6">${element.gender}</p>
+          <p class="subtitle is-6">${element.cell}</p>
+        </div>
+      </div>
+      
+    
+      </div>
+      </div>
+      ` 
+    
+  });
+
+})
+.catch(error=>console.log(error))
+
+})
+
+
+
+
+   
